0f9d1bfb4aa1ce2ef3e3af80921f8d8e86f967f2
Vasily Mikhaylov <vasyanch852@gmail.com>
2018-11-10 19:45:04 +0300

    Attempt 2.1

--- hw1/log_analyzer/log_analyzer.py
+++ hw1/log_analyzer/log_analyzer.py
@@ -16,7 +16,8 @@ import argparse
 import configparser
 import logging
 import os
-from collections import namedtuple
+from datetime import datetime
+from collections import namedtuple, defaultdict
 from string import Template
 from fractions import Fraction
 
@@ -29,6 +30,8 @@ config = {
     "LOGGING_LEVEL": logging.DEBUG,
     "LOGGING_TO_FILE": None
 }
+Log = namedtuple('log', 'file_for_analyze date ex')
+Req = namedtuple('req', 'url time')
 
 
 def create_parser():
@@ -46,7 +49,7 @@ def parse_config(default_config, path):
            ((key == 'report_size' and int(value) > 0) or (key == 'level_parse' and 0 < int(value) <= 100)):
             outer_config[key] = int(value)
             check = 1
-        elif key in ('report_dir', 'log_dir', 'logging_to_file') and os.path.exists(value):
+        elif (key == 'log_dir' and os.path.exists(value)) or (key in ('logging_to_file', 'report_dir')):
             check = 1
         elif key == 'logging_level' and value in ('INFO', 'DEBUG', 'ERROR'):
             check = 1
@@ -72,38 +75,32 @@ def parse_config(default_config, path):
                 if priority_config:
                     default_config[item[0].upper()] = priority_config[item[0]]
                 else:
-                    return 'bad option:{} in config file!'.format(item[0].upper())
+                    raise Exception, 'Bad option:{} in config file!'.format(item[0].upper())
             return default_config
         except Exception:
-            return 'bad config!'
+            raise Exception, "Bad config!"
     else:
-        return 'Bad path to config:{}'.format(path) 
+        raise Exception, "Bad path to config!"
 
 
 def find_log(dir_log_nginx):
-    log = namedtuple('log', 'file_for_analyze date ex')
-    maximum = 0
-    file_for_analyze = ''
+    maximum = datetime.strptime('18000101', '%Y%m%d')
+    file_for_analyze = None
     date = 0
     ex = None
     for file_ in os.listdir(dir_log_nginx):
-        if re.match(r'nginx-access-ui.log-\d{8}((\.gz)|$)', file_):
-            date_and_ex = file_.split('-')[-1]
-            date_and_ex = date_and_ex.split('.')
-            cur_date = int(date_and_ex[0])
+        f = re.match(r'nginx-access-ui.log-(?P<cur_date>\d{8})(\.(?P<ex>gz)|$)', file_)
+        if f:
+            cur_date, ex = datetime.strptime(f.group('cur_date'), '%Y%m%d'), f.group('ex')
             maximum = max(maximum, cur_date)
             if cur_date == maximum:
                 file_for_analyze = file_
-                try:
-                    date, ex = cur_date, date_and_ex[1]
-                except IndexError:
-                    date, ex = cur_date, None
-    return log(file_for_analyze, date, ex)
+                date, ex = cur_date, ex
+    return Log(file_for_analyze, date, ex) if file_for_analyze else None
 
 
 def parse_string(file_from):
-    req = namedtuple('req', 'url time')
-    for line in file_from.readlines():
+    for line in file_from:
         lst = line.split()
         pattern_ip = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
         pattern_url = r'(/\w*)+'
@@ -111,33 +108,27 @@ def parse_string(file_from):
         try:
             if re.match(pattern_ip, lst[0]) and re.match(pattern_url, lst[6]) and re.match(pattern_time, lst[-1]):
                 url, time = lst[6], float(lst[-1])
-                yield req(url, time)
+                yield Req(url, time)
             else:
-                yield 'bad string'
+                yield None
         except IndexError:
-            yield 'bad string'
+            yield None
 
 
 def parse_log(path_file_for_analyze, ex):
-    if ex is None:
-        file_log = open(path_file_for_analyze, 'r')
-    else:
-        file_log = gzip.open(path_file_for_analyze, 'rb')
-    data = dict()
+    file_open = open if ex is None else gzip.open
+    data = defaultdict(list)
     all_time = 0
     all_strings = 0
     good_strings = 0
-    for string_log in parse_string(file_log):
-        all_strings += 1
-        if string_log != 'bad string':
-            good_strings += 1
-            all_time += string_log.time
-            key = string_log.url
-            if key not in data:
-                data[key] = [string_log.time]
-            else:
+    with file_open(path_file_for_analyze, 'r') as log_file:
+        for string_log in parse_string(log_file):
+            all_strings += 1
+            if string_log is not None:
+                good_strings += 1
+                all_time += string_log.time
+                key = string_log.url
                 data[key].append(string_log.time)
-    file_log.close()
     return data, good_strings, all_strings, all_time
 
 
@@ -175,63 +166,57 @@ def count_stat(data,  num_req, all_time, report_size=5):  # data ->{url: [list_o
     return data_to_render_
 
 
-def render_report(data_to_render_, date, report_dir):
+def render_report(data_to_render_, report_path):
     table_json = json.dumps(data_to_render_, indent=4)
-    report = open('report.html', 'r')
-    report_to_file = Template(report.read())
-    report.close()
+    with open('report.html', 'r') as report:
+        report_to_file = Template(report.read())
     report_to_file = report_to_file.safe_substitute(table_json=table_json)
-    path_report = '{0}/report-{1}.{2}.{3}.html'.format(report_dir, date[0:4], date[4:6], date[6:8])
-    report_date = open(path_report, 'w')
-    report_date.write(report_to_file)
-    report_date.close()
-    return path_report
+    with open(report_path, 'w') as report_date:
+        report_date.write(report_to_file)
 
 
 def main(config):
     file_log = find_log(config["LOG_DIR"])
-    if file_log.file_for_analyze != '':
-        last_file_log = open('last_file_log', 'r')
-        if file_log.file_for_analyze == last_file_log.read():
-            logging.error('the file:{} alredy processed'.format(file_log.file_for_analyze))
-            last_file_log.close()
-            sys.exit(1)
-        last_file_log.close()
-    else:
-        logging.error('file_for_analyze is not found.')
-        sys.exit(1)
+    if file_log is None:
+        logging.error('File_for_analyze is not found.')
+        return
     logging.debug("find_log: {} OK".format(file_log.file_for_analyze))
-        
-    path_file_for_analyze = '{0}/{1}'.format(config["LOG_DIR"], file_log.file_for_analyze)
+    report = 'report-{}.html'.format(file_log.date.strftime('%Y.%m.%d'))
+    if os.path.exists(config['REPORT_DIR']):
+        if report in os.listdir(config['REPORT_DIR']):
+            logging.error('the file:{} already processed'.format(file_log.file_for_analyze))
+            return
+    else:
+        try:
+            os.mkdir(config['REPORT_DIR'])
+        except:
+            logging.error('Bad option:REPORT_DIR in config file!')
+            return
+    report_path = os.path.join(config['REPORT_DIR'], report)
+    path_file_for_analyze = os.path.join(config["LOG_DIR"], file_log.file_for_analyze)
     raw_data, good_strings, all_strings, all_time = parse_log(path_file_for_analyze, file_log.ex)
+    print all_strings, good_strings
     if all_strings > 0:
         if Fraction(good_strings, all_strings) <= Fraction(config["LEVEL_PARSE"], 100):
             logging.error('Could not parse more {0}% in {1}. \
                             Try to check log format.'.format(config["LEVEL_PARSE"],
                                                               path_file_for_analyze))
-            sys.exit(1)
+            return
     else:
         logging.error('{} file is empty!'.format(path_file_for_analyze))
-        sys.exit(1)
+        return
     logging.debug("parse_log: OK")
     data_to_render = count_stat(raw_data, good_strings, all_time,
                                     report_size=config["REPORT_SIZE"])
     logging.debug("count_stat: OK")
-    path_report = render_report(data_to_render, str(file_log.date), report_dir=config["REPORT_DIR"])
-    logging.info("render_report: OK. Report file: {}".format(path_report))
-    last_file_log = open('last_file_log', 'w')
-    last_file_log.write(file_log.file_for_analyze)
-    last_file_log.close()
-    logging.debug("last_file_log is update: OK")
+    render_report(data_to_render, report_path)
+    logging.info("render_report: OK. Report file: {}".format(report_path))
 
 
 if __name__ == "__main__":
     parser = create_parser()
     namespace = parser.parse_args()
     config = parse_config(config, namespace.config)  # type(namespace.config) -> str
-    if type(config) is not dict:
-        raise Exception, config
-
     logging.basicConfig(format='[%(asctime)s] %(levelname).1s %(message)s', level=config["LOGGING_LEVEL"],
                         filename=config["LOGGING_TO_FILE"])
     try:
