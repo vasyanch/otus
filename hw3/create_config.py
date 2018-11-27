ade6bcbfa618dae94c3ec65426da5f25c723c7fd
Vasily Mikhaylov <vasyanch852@gmail.com>
2018-11-28 01:13:38 +0300

    Attempt 3.2 hw3

--- hw3/create_config.py
+++ hw3/create_config.py
@@ -9,7 +9,7 @@ def create_config(path):
     config.add_section('Config_api')
     config.set('Config_api', 'LOGGING_TO_FILE', 'api.log')
     config.set('Config_api', 'PORT', '8080')
-    config.set('Config_api', 'LOGGING_LEVEL', 'DEBUG')
+    config.set('Config_api', 'LOGGING_LEVEL', 'INFO')
 
     with open(path, 'w') as config_file:
         config.write(config_file)
@@ -18,6 +18,3 @@ def create_config(path):
 if __name__ == '__main__':
     path = './config_api'
     create_config(path)
-    import os
-    print  os.path.join(os.path.dirname(os.path.abspath(__file__)), 'REPORT_DIR')
-    print os.path.join(os.path.dirname(os.path.abspath(__file__)), '/home')
